35b83decac0cb77c823aed205add533e1a9584c8
anulatha <anulatha.padmakumari@gmail.com>
2016-09-28 16:51:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/Article one', function (req,res){
+    res.send('Article one is served here for you');
+    
+});
+app.get('/Article two', function (req,res){
+    res.send('Article two is served here for you');
+    
+});
+app.get('/Article Three', function (req,res){
+    res.send('Article three is served here for you');
+    
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
